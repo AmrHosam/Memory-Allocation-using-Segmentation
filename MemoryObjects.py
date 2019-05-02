044d022b582905de4c516f59891525830fed6c43
Omar Shafik <o.shafik13@gmail.com>
2019-05-02 19:33:12 +0200

    Memory Class + modifications

--- MemoryObjects.py
+++ MemoryObjects.py
@@ -1,28 +1,64 @@
-class Segment:
+import copy
+
+
+class Segment:  # this class should be used by Allocator and Deallocator
+
+    # base parameter should be set only by the Allocator Algorithm. free parameter should be set only by Deallocator Algorithm
     def __init__(self, name, length, parent_process, base=-1, free=False):
         self.parentProcess = parent_process
-        self.base = base
-        self.length = length
-        self.free = free
-        self.name = name
-
-    def reset(self, base, length, free, name):
-        self.base = base
-        self.length = length
-        self.free = free
-        self.name = name
+        self.base = copy.deepcopy(base)
+        self.length = copy.deepcopy(length)
+        self.free = copy.deepcopy(free)
+        self.name = copy.deepcopy(name)
 
     def deallocate(self):
+        self.parentProcess = 0
         self.free = True
         self.name = "hole"
 
+    def __eq__(self, seg):
+        return (self.name == seg.name and self.parentProcess.name == seg.parentProcess.name)
 
-class Process:
-    def __init__(self, segments):
+
+class Process:  # this class should only be used by Deallocator
+    count = 0
+
+    def __init__(self):
+        self.name = "UT"+str(Process.count)
+        Process.count += 1
         self.segments = []
+
+    def addSegments(self, segments):
         for segment in segments:
             self.segments.append(segment)
 
     def deallocate(self):
         for segment in self.segments:
             segment.deallocate()
+
+
+class Memory:
+    def __init__(self, segments):
+        self.segments = []
+        for segment in segments:
+            self.segments.append(copy.deepcopy(segment))
+
+    def allocate(self, segment):
+        # Amr
+        pass
+
+    def deallocate(self, process):
+        process.deallocate()
+        prevSegment = Segment("a", 0, Process())
+        Process.count -= 1
+        prevSegment.parentProcess.name = "UT"
+        i = 0
+        for segment in self.segments:
+            if segment == prevSegment:
+                prevSegment.length += segment.length
+                self.segments.remove(segment)
+                i += 1
+                continue
+            prevSegment = segment
+            i += 1
+        pass
