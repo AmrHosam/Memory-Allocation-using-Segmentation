2ef741eef08c5de9066409dee2ebe201c7d70998
Omar Shafik <o.shafik13@gmail.com>
2019-05-02 10:51:53 +0200

    Segment Class

---
+++ SegmentClass.py
@@ -0,0 +1,6 @@
+class Segment:
+    def __init__(self, base=0, length=0, free=False, name="null"):
+        self.base = base
+        self.length = length
+        self.free = free
+        self.name = name
