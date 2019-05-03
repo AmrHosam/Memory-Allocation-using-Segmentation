e314a1d90bc2d8369ac340eb785609e218d1693a
Omar Shafik <o.shafik13@gmail.com>
2019-05-03 17:18:17 +0200

    Modifications and Commenting

--- MemoryObjects.py
+++ MemoryObjects.py
@@ -1,8 +1,13 @@
 import copy
 
+# The third window (segments window) creates a parent process and its segments, then calls the allocator method.
+# The Segment window should return the new memory object.
+
 
 class Segment:  # this class should be used by Allocator and Deallocator
 
+    # before creating a segment a parent process must be created. In the segments window only one new process should be created
+    # constructor automatically appends the new segment to the parent process's list of segments
     # base parameter should be set only by the Allocator Algorithm. free parameter should be set only by Deallocator Algorithm
     def __init__(self, name, length, parent_process, base=-1, free=False):
         self.parentProcess = parent_process
@@ -10,6 +15,7 @@ class Segment:  # this class should be used by Allocator and Deallocator
         self.length = copy.deepcopy(length)
         self.free = copy.deepcopy(free)
         self.name = copy.deepcopy(name)
+        self.parentProcess.segments.append(self)
 
     def deallocate(self):
         self.parentProcess = 0
@@ -28,10 +34,6 @@ class Process:  # this class should only be used by Deallocator
         Process.count += 1
         self.segments = []
 
-    def addSegments(self, segments):
-        for segment in segments:
-            self.segments.append(segment)
-
     def deallocate(self):
         for segment in self.segments:
             segment.deallocate()
@@ -39,24 +41,36 @@ class Process:  # this class should only be used by Deallocator
 
 class Memory:
     def __init__(self, segments):
+        # List that holds all segments of Memory
         self.segments = []
+
+        # List that holds all (newly) allocated processes in Memory
+        self.processes = []
+
         for segment in segments:
             self.segments.append(copy.deepcopy(segment))
 
-    def allocate(self, segment):
-        # Amr
+    def allocate(self, process):
+
+        # Amr: Add allocator algorithm here.
+
+        # Amr: if process is added succesfully
+        # self.processes.append(process)
+
         pass
 
     def deallocate(self, process):
         process.deallocate()
+        self.processes.remove(process)
+        del process  # possible bug
         prevSegment = Segment("a", 0, Process())
         Process.count -= 1
         prevSegment.parentProcess.name = "UT"
-        i = 0
+        # i = 0
         for segment in self.segments:
             if segment == prevSegment:
                 prevSegment.length += segment.length
                 self.segments.remove(segment)
                 segment = prevSegment
             prevSegment = segment
-            i += 1
+            # i += 1
